b9e82318605d36b74d9a36d8cfdf9e49cd80696e
Robin Bygrave <robin.bygrave@gmail.com>
2016-06-17 22:48:20 +1200

    Add MainDbMigration

---
+++ src/test/java/main/MainDbMigration.java
@@ -0,0 +1,27 @@
+package main;
+
+import com.avaje.ebean.config.dbplatform.DbPlatformName;
+import com.avaje.ebean.dbmigration.DbMigration;
+
+import java.io.IOException;
+
+/**
+ * Generate the DB Migration.
+ */
+public class MainDbMigration {
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+
+    //System.setProperty("ddl.migration.version", "1.2_33");
+    //System.setProperty("ddl.migration.name", "");
+
+    //System.setProperty("ddl.migration.pendingDropsFor", "1.1");
+    //System.setProperty("ddl.migration.pendingDrop", "1.1");
+
+    DbMigration dbMigration = new DbMigration();
+    dbMigration.setPlatform(DbPlatformName.POSTGRES);
+
+    // generate the migration xml and ddl
+    dbMigration.generateMigration();
+  }
+}
